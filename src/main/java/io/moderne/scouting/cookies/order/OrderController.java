d31658e337bbf55a98953db4b919272783c2b3fb
Peter Streef <p.streef@gmail.com>
2024-02-25 13:31:46 +0100

    clean

--- src/main/java/io/moderne/scouting/cookies/order/OrderController.java
+++ src/main/java/io/moderne/scouting/cookies/order/OrderController.java
@@ -1,6 +1,5 @@
 package io.moderne.scouting.cookies.order;
 
-import io.moderne.scouting.cookies.CookieType;
 import io.moderne.scouting.cookies.error.ApiError;
 import io.moderne.scouting.cookies.error.ApiException;
 import io.moderne.scouting.cookies.error.ErrorHandling;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 
 @RestController
