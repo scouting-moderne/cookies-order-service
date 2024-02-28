144a56770815ce26f200a665c3a83d03c5e61863
Peter Streef <p.streef@gmail.com>
2024-02-28 10:57:59 +0000

    refactor: Add a literal method argument
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.java.AddLiteralMethodArgument?defaults=W3sibmFtZSI6ImFyZ3VtZW50SW5kZXgiLCJ2YWx1ZSI6MH1d&organizationId=YjU2YzBkZTItMGNjMy00YThhLWJlY2ItNDI5MGFjNjZiZjcy
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/io/moderne/scouting/cookies/order/OrderController.java
+++ src/main/java/io/moderne/scouting/cookies/order/OrderController.java
@@ -22,7 +22,7 @@ class OrderController {
     @PostMapping
     public Order createOrder(@RequestBody OrderRequest order) {
         User user = userClient.findUser(order.userId())
-                .orElseThrow(() -> new ApiException(new ApiError("User", "The user does not exist.")));
+                .orElseThrow(() -> new ApiException(new ApiError("User", "The user does not exist.", 500)));
         return orderService.createOrder(user, order.reservationId());
     }
 
